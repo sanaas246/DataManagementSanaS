1f1f9bf2dd246304e8331cd511b43b329798eee7
sanaas246 <112659421+sanaas246@users.noreply.github.com>
2022-09-28 14:46:38 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# MOVIE DATA MANAGEMENT PROJECT - SANA S
+
+
+# Pre-existing Users Dictionary (Username, Password)
+
+# Previously Added Movies 
+
+# Movie Dictionary (Title, Genre, Director)
+
+# User System
+
+# Menu Option Selection 
+
+# Menu Options 
+
+# Menu Functions 
